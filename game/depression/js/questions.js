648f95e5cb4fac34ffb2dc44ffb70231c4ceed9c
Aditya-Ojha <ojhaaditya576@gmail.com>
2022-03-23 02:34:29 +0530

    depression added

---
+++ game/depression/js/questions.js
@@ -0,0 +1,139 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    block: "nil",
+    question: "Little interest or pleasure in doing things",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 2,
+    block: "nil",
+    question: "Feeling down, depressed, or hopeless",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 3,
+    block: "nil",
+    question: "Trouble falling or staying asleep, or sleeping too much",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 4,
+    block: "nil",
+    question: "Feeling tired or having little energy",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 5,
+    block: "nil",
+    question: "Poor appetite or overeating",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 6,
+    block: "nil",
+    question: "Feeling bad about yourself - or that you are a failure or have let yourself or your family down",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 7,
+    block: "nil",
+    question: "Trouble concentrating on things, such as reading the newspaper or watching television",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 8,
+    block: "nil",
+    question: "Moving or speaking so slowly that other people could have noticed",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 9,
+    block: "nil",
+    question: "Thoughts that you would be better off dead, or of hurting yourself",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+  {
+    numb: 10,
+    block: "nil",
+    question: "If you've had any days with issues above, how difficult have these problems made it for you at work, home, school, or with other people?",
+    answer: "2",
+    options: [
+      "Not at all",
+      "Several days",
+      "More than half of the days",
+      "Nearly every day"
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
