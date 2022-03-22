360a01b45dbf801acd9035a22a6adca70db2f68f
Aditya-Ojha <ojhaaditya576@gmail.com>
2022-03-23 02:27:36 +0530

    dyscalculia added

---
+++ game/dyscalculia/js/questions.js
@@ -0,0 +1,79 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    block: "nil",
+    question: "Which is greater",
+    answer: "9",
+    options: [
+      "3",
+      "8",
+      "9",
+      "1"
+    ]
+  },
+  {
+    numb: 2,
+    block: "nil",
+    question: "Which is smaller",
+    answer: "0",
+    options: [
+      "5",
+      "0",
+      "1",
+      "2"
+    ]
+  },
+  {
+    numb: 3,
+    block: "nil",
+    question: "3+5 = ?",
+    answer: "8",
+    options: [
+      "2",
+      "35",
+      "4",
+      "8"
+    ]
+  },
+  {
+    numb: 4,
+    block: "nil",
+    question: "5-3 = ?",
+    answer: "2",
+    options: [
+      "2",
+      "35",
+      "4",
+      "8"
+    ]
+  },
+  {
+    numb: 5,
+    block: "nil",
+    question: "9-6 = ?",
+    answer: "3",
+    options: [
+      "13",
+      "1",
+      "3",
+      "10"
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
